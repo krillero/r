14daca054ab529bbe846c09ec318afa92fc5e6db
Krillero <krille88@gmail.com>
2018-10-30 22:55:25 +0100

    test

---
+++ test.r
@@ -0,0 +1,6 @@
+#Under en period på 90 dagar, kolla om aktien passerat medelvärdet (mean) minst 5 ggr
+#Kolla om aktier korrelerar med varandra om man förskjuter ena raden (se in i framtiden)
+
+
+#plot OHLC
+#plot_ly(x = Date, type="ohlc", open = Openprice, close = Closeprice, high = Highprice, low = Lowprice)
