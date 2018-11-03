3cb2ee3cb9292a02b4f34e091de94ebe9fd2bf72
Krillero <krille88@gmail.com>
2018-11-03 18:28:12 +0100

    test

--- test.r
+++ test.r
@@ -1,6 +1,7 @@
 #Under en period på 90 dagar, kolla om aktien passerat medelvärdet (mean) minst 5 ggr
 #Kolla om aktier korrelerar med varandra om man förskjuter ena raden (se in i framtiden)
 
+#dela upp utdelningsaktier så att de ger utdelning varje månad
 
 #plot OHLC
 #plot_ly(x = Date, type="ohlc", open = Openprice, close = Closeprice, high = Highprice, low = Lowprice)
